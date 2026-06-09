108824854d8c371fb00fd6f825e1c92b581e4041
CFMAD <formation@serfam.be>
2026-06-09 16:19:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -85,27 +85,28 @@ ligne.style.borderLeft =
 `12px solid ${couleur}`;
 
 ligne.innerHTML = `
-<div class="planning-card-content">
+<div class="planning-header">
 
-<div class="planning-top">
-    <span class="planning-heure">${item.debut} → ${item.fin}</span>
+    <span class="planning-time">
+        ${item.debut} - ${item.fin}
+    </span>
+
+    <span class="planning-room"
+          style="background:${couleur}">
+        ${item.salle}
+    </span>
+
 </div>
 
-<div class="planning-formation">
+<div class="planning-title">
     ${item.formation}
 </div>
 
-<div class="planning-salle"
-     style="background:${couleur}">
-     ${item.salle}
-</div>
-
-<div class="planning-formateur">
+<div class="planning-trainer">
     👤 ${item.formateur}
 </div>
-
-</div>
 `;
+              
                 
                 planning.appendChild(ligne);
             });
